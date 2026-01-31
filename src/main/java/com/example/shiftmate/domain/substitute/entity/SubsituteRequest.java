e82905a460e15a551dca4a401328c743ae8f934a
fhdl1221 <fhdl353212@gmail.com>
2026-01-31 22:38:57 +0900

    feat: 대타 요청/지원 entity 생성

---
+++ src/main/java/com/example/shiftmate/domain/substitute/entity/SubsituteRequest.java
@@ -0,0 +1,31 @@
+package com.example.shiftmate.domain.substitute.entity;
+
+import com.example.shiftmate.domain.schedule.entity.Schedule;
+import com.example.shiftmate.domain.substitute.status.RequestStatus;
+import com.example.shiftmate.domain.user.entity.User;
+import com.example.shiftmate.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter@Table(name = "subsitute_requests")
+@NoArgsConstructor
+public class SubsituteRequest extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long subsitute_id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule; // 대타 요청 스케줄
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requester_id")
+    private User requester; // 대타 요청자
+
+    @Enumerated(EnumType.STRING)
+    private RequestStatus status = RequestStatus.OPEN; // 대타 요청 상태
+    private String reason; // 대타 요청 이유
+}
---
+++ src/main/java/com/example/shiftmate/domain/substitute/entity/SubstituteApplication.java
@@ -0,0 +1,30 @@
+package com.example.shiftmate.domain.substitute.entity;
+
+import com.example.shiftmate.domain.substitute.status.ApplicationStatus;
+import com.example.shiftmate.domain.user.entity.User;
+import com.example.shiftmate.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter@Table(name = "subsitute_applications")
+@NoArgsConstructor
+
+public class SubstituteApplication extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long application_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "request_id")
+    private SubsituteRequest request; // 어떤 대타 요청을 지원한건지
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "applicant_id")
+    private User applicant; // 지원한 사람
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus status = ApplicationStatus.WAITING; // 대타 요청 지원 상태
+}
