1752bf5ff471cd28a84b077e4909a03839f5b126
jintheog <jintheog11@gmail.com>
2026-02-02 10:54:59 +0900

    Security 파일들 추가

---
+++ src/main/java/com/example/shiftmate/global/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.example.shiftmate.global.config;
+
+public class SecurityConfig {
+
+}
---
+++ src/main/java/com/example/shiftmate/global/security/CustomUserDetailsService.java
@@ -0,0 +1,4 @@
+package com.example.shiftmate.global.security;
+
+public class CustomUserDetailsService {
+}
