225eae098c41708a39c9375b21466a3e863830ea
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2026-02-02 15:12:20 +0900

    feat/store repository 작성

---
+++ src/main/java/com/example/shiftmate/domain/store/repository/StoreRepository.java
@@ -0,0 +1,8 @@
+package com.example.shiftmate.domain.store.repository;
+
+import com.example.shiftmate.domain.store.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+}
