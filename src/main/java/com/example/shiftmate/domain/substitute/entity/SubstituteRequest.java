23e8a7d1325f42e5d6efb37ed02a53233bac3795
fhdl1221 <fhdl353212@gmail.com>
2026-02-02 10:29:28 +0900

    refactor: substitute 엔티티 필드 타입 수정

--- src/main/java/com/example/shiftmate/domain/substitute/entity/SubstituteRequest.java
+++ src/main/java/com/example/shiftmate/domain/substitute/entity/SubstituteRequest.java
@@ -1,10 +1,11 @@
 package com.example.shiftmate.domain.substitute.entity;
 
 import com.example.shiftmate.domain.shiftAssignment.entity.ShiftAssignment;
+import com.example.shiftmate.domain.storeMember.entity.StoreMember;
 import com.example.shiftmate.domain.substitute.status.RequestStatus;
-import com.example.shiftmate.domain.user.entity.User;
 import com.example.shiftmate.global.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,14 +21,23 @@ public class SubstituteRequest extends BaseTimeEntity {
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "shiftassignment_id")
-    private ShiftAssignment shiftassignment; // 대타 요청 스케줄
+    @JoinColumn(name = "shiftassignment_id", nullable = false)
+    private ShiftAssignment shiftAssignment; // 대타 요청 스케줄
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "requester_id")
-    private User requester; // 대타 요청자
+    @JoinColumn(name = "requester_id", nullable = false)
+    private StoreMember requester; // 대타 요청자 (해당 매장의 멤버만 신청 가능)
 
     @Enumerated(EnumType.STRING)
-    private RequestStatus status = RequestStatus.OPEN; // 대타 요청 상태
+    private RequestStatus status; // 대타 요청 상태
+
     private String reason; // 대타 요청 이유
+
+    @Builder
+    public SubstituteRequest(ShiftAssignment shiftAssignment, StoreMember requester, RequestStatus status, String reason) {
+        this.shiftAssignment = shiftAssignment;
+        this.requester = requester;
+        this.status = status;
+        this.reason = reason;
+    }
 }
