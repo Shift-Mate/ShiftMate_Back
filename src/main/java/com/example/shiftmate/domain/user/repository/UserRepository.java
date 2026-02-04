3e85387b488ed91e5474deaadef8fa8e6c850007
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2026-02-04 17:31:43 +0900

    chore:user repository 삭제

--- src/main/java/com/example/shiftmate/domain/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shiftmate.domain.user.repository;
-
-import com.example.shiftmate.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
