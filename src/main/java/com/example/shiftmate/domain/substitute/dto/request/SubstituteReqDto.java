a3b6aa6928e601d6c00ba0136455b22438a37f12
fhdl1221 <fhdl353212@gmail.com>
2026-02-05 15:40:33 +0900

    refactor: 대타 요청 사유 길이 제한 추가

--- src/main/java/com/example/shiftmate/domain/substitute/dto/request/SubstituteReqDto.java
+++ src/main/java/com/example/shiftmate/domain/substitute/dto/request/SubstituteReqDto.java
@@ -2,6 +2,7 @@ package com.example.shiftmate.domain.substitute.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,5 +13,7 @@ public class SubstituteReqDto {
     @NotNull
     private Long assignmentId;
 
+    @NotBlank(message = "사유를 입력해주세요.")
+    @Size(max = 200, message = "사유는 200자 이내로 입력해주세요.")
     private String reason;
 }
