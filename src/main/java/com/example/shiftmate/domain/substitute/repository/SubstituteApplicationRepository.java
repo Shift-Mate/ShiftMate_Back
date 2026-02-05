be5694a8fba5eefe795479850d66faa5a7d47261
fhdl1221 <fhdl353212@gmail.com>
2026-02-06 00:32:11 +0900

    feat: 본인 대타 지원 내역 조회를 위한 메서드 작성

--- src/main/java/com/example/shiftmate/domain/substitute/repository/SubstituteApplicationRepository.java
+++ src/main/java/com/example/shiftmate/domain/substitute/repository/SubstituteApplicationRepository.java
@@ -12,4 +12,16 @@ public interface SubstituteApplicationRepository extends JpaRepository<Substitut
 
     boolean existsByRequestIdAndApplicantId(Long requestId, Long id);
 
+    // 내 지원 내역 조회
+    @Query("SELECT sa FROM SubstituteApplication sa " +
+            "JOIN FETCH sa.request r " +             // 대타 요청
+            "JOIN FETCH r.shiftAssignment s " +      // 스케줄
+            "JOIN FETCH s.shiftTemplate " +          // 시간 정보 조회를 위한 템플릿
+            "JOIN FETCH r.requester req " +          // 요청자
+            "JOIN FETCH req.user " +                 // 요청자 이름
+            "JOIN FETCH sa.applicant app " +         // 지원자
+            "JOIN FETCH app.user " +                 // 지원자 이름
+            "WHERE sa.applicant.id = :applicantId " +
+            "ORDER BY sa.createdAt DESC")
+    List<SubstituteApplication> findAllByApplicantId(@Param("applicantId") Long applicantId);
 }
