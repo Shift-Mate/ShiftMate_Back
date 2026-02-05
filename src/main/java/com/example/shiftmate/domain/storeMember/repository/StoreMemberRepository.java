0eb8b0f9561c6a068e94f344556c4b556261b8fb
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2026-02-05 10:17:45 +0900

    feat:StoreMember repository 생성

--- src/main/java/com/example/shiftmate/domain/storeMember/repository/StoreMemberRepository.java
+++ src/main/java/com/example/shiftmate/domain/storeMember/repository/StoreMemberRepository.java
@@ -1,7 +1,10 @@
 package com.example.shiftmate.domain.storeMember.repository;
 
 import com.example.shiftmate.domain.storeMember.entity.StoreMember;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreMemberRepository extends JpaRepository<StoreMember, Long> {
+    // 같은 매장에 같은 사람이 등록되어있는지(중복 체크)
+    Optional<StoreMember> findByStoreIdAndUserId(Long storeId, Long userId);
 }
