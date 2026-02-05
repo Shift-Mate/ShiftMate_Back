a9646a474182da4af935c62c2090e439f217e61e
fhdl1221 <fhdl353212@gmail.com>
2026-02-05 15:10:49 +0900

    feat: 매장 직원 조회 메서드 작성

--- src/main/java/com/example/shiftmate/domain/storeMember/repository/StoreMemberRepository.java
+++ src/main/java/com/example/shiftmate/domain/storeMember/repository/StoreMemberRepository.java
@@ -1,7 +1,12 @@
 package com.example.shiftmate.domain.storeMember.repository;
 
 import com.example.shiftmate.domain.storeMember.entity.StoreMember;
+import com.example.shiftmate.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StoreMemberRepository extends JpaRepository<StoreMember, Long> {
+
+    Optional<StoreMember> findByStoreIdAndUserId(Long storeId, Long userId);
 }
