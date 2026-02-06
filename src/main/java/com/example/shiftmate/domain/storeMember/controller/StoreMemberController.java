1d85ec598b7fd6f48737e85ecc70b7d20d3c8e14
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2026-02-06 16:38:26 +0900

    feat: StoreMemeber Controller delete 기능 구현

--- src/main/java/com/example/shiftmate/domain/storeMember/controller/StoreMemberController.java
+++ src/main/java/com/example/shiftmate/domain/storeMember/controller/StoreMemberController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -88,4 +89,9 @@ public class StoreMemberController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> deleteStoreMember(@PathVariable Long id) {
+        storeMemberService.delete(id);
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
 }
