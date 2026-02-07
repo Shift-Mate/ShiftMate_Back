9aa2d6878c6c95c1a2ff55ea9d270e71c8c93419
ghkdquddlf <xhdlapdlxm12@naver.com>
2026-02-07 17:26:11 +0900

    refactor: 요일 정보의 타입을 Integer -> DayOfWeek(Enum) 로 변경

--- src/main/java/com/example/shiftmate/domain/employeePreference/dto/request/CreatePreferenceItemReqDto.java
+++ src/main/java/com/example/shiftmate/domain/employeePreference/dto/request/CreatePreferenceItemReqDto.java
@@ -1,6 +1,7 @@
 package com.example.shiftmate.domain.employeePreference.dto.request;
 
 import com.example.shiftmate.domain.employeePreference.entity.PreferenceType;
+import java.time.DayOfWeek;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreatePreferenceItemReqDto {
 
-    private Integer dayOfWeek;
+    private DayOfWeek dayOfWeek;
     private PreferenceType type;
     private Long templateId;
 
