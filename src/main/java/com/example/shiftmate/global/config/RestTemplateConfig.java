5a5d6617e4c668b155f6ca8d9740880d754afee7
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2026-02-09 22:54:27 +0900

    feat:RestTmplateConfig 작성: 외부 api 호출용 restTemplate를 표준 설정으로 만들어주는 클래스

---
+++ src/main/java/com/example/shiftmate/global/config/RestTemplateConfig.java
@@ -0,0 +1,17 @@
+package com.example.shiftmate.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);  // 연결 타임아웃 5초
+        factory.setReadTimeout(5000);     // 읽기 타임아웃 5초
+        return new RestTemplate(factory);
+    }
+}
