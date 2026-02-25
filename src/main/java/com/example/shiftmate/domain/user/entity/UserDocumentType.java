3feafb74bdf07e622204b8cc59e85f6a34b74b79
jintheog <jintheog11@gmail.com>
2026-02-25 14:49:49 +0900

    feature: bank account info file upload

--- src/main/java/com/example/shiftmate/domain/user/entity/UserDocumentType.java
+++ src/main/java/com/example/shiftmate/domain/user/entity/UserDocumentType.java
@@ -2,5 +2,6 @@ package com.example.shiftmate.domain.user.entity;
 
 public enum UserDocumentType {
     HEALTH_CERTIFICATE, // 보건증
-    IDENTIFICATION  // 신분증
+    IDENTIFICATION,     // 신분증
+    BANKBOOK_COPY       // 통장 사본
 }
